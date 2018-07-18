49d6b1b9e7bbfc2f17446167a40d477aa130a28b
Justin Newby <jnewby91@gmail.com>
2018-07-18 09:31:00 -0400

    first commit to file I have already  integrated LastFM and iTunes API and console log songpreviewURL

---
+++ index.js
@@ -0,0 +1,51 @@
+const lastFmURL = 'https://ws.audioscrobbler.com/2.0/';
+const iTunesURL = 'https://itunes.apple.com/search?';
+
+function getLastFMdata(searchTerm, callback) {
+	const param = {
+    method: 'artist.getsimilar',
+		api_key: 'bfe9c08eaf78508d5aa1f2009aadeaae',
+        artist: `${searchTerm}`,
+        limit: 10,
+        format: 'json'
+	};
+	 $.getJSON(lastFmURL,param, callback);
+}
+
+function getiTunesdata(searchTerm, callback ){ 
+    const param = {
+      term: encodeURIComponent(searchTerm).replace(/%20/g, '+'), 
+      country: 'US',
+      limit : 5
+    }
+    $.getJSON(iTunesURL,param, callback);
+  }
+
+function firstCall(result) {
+    console.log(result);
+    const likeArtist = result.similarartists.artist[0].name;
+    console.log(likeArtist);
+
+    getiTunesdata(likeArtist, secondCall);
+
+}
+
+function secondCall(result){
+    console.log(result.results[0].previewUrl);
+   // const songPreview = '';
+
+}
+
+
+function getData() {
+	$('.searchForm').submit(function(event) {
+		event.preventDefault();
+		const searchVal = $('#search').val();
+		console.log(searchVal);
+		getLastFMdata(searchVal, firstCall);
+	});
+}
+
+
+
+$(getData);
